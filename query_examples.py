08142f0850211925d2cd0d07ee42766fb82749f4
Darren Ng <darren.ng@versed.ai>
2021-07-15 15:45:47 +0100

    added more query examples

--- query_examples.py
+++ query_examples.py
@@ -35,17 +35,62 @@ WHERE
 LIMIT 20
 """
 
-example_query4 = """# Brazilians who are either poets or novelist, but not both
-SELECT ?item ?itemLabel 
+# adding "Label" at the end of a variable will show the variable in its label, instead of a Qcode link.
+# to show entries that don't have a birthplace, replace "?item wdt:P19 ?birthplace ." with 
+# "OPTIONAL { ?item wdt:P19 ?birthplace .}"
+example_query4 = """# Brazilians who are either poets or novelist, but not both and show their birth places
+SELECT ?item ?itemLabel ?birthplaceLabel
 WHERE 
 {
   ?item wdt:P31 wd:Q5. # humans
-  ?item wdt:P27 wd:Q155.  # citizens of Brazil
+  ?item wdt:P27 wd:Q155. # citizens of Brazil
   { ?item wdt:P106 wd:Q49757.} # occupation - poet
   UNION 
   { ?item wdt:P106 wd:Q6625963. } # occupation - novelist
+  ?item wdt:P19 ?birthplace. # and put the birthplace into a variable
   
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
 }
-LIMIT 20
+"""
+
+example_query5 = """# American politicians whose father was also a politician
+
+# instance of human
+# citizenship - US
+# occupation - politician
+# father - ?father
+
+SELECT ?item ?itemLabel ?fatherLabel
+WHERE 
+{
+  ?item wdt:P31 wd:Q5. # instance of human
+  ?item wdt:P27 wd:Q30. # US citizen
+  ?item wdt:P106 wd:Q82955. # politician
+  ?item wdt:P22 ?father. # who have *a* father
+  ?father wdt:P106 wd:Q82955. # who *himself* was a politician
+  
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+}
+"""
+
+example_query5 = """# American politicians whose father was also a politician
+# with child born after 1950
+# instance of human
+# citizenship - US
+# occupation - politician
+# father - ?father
+
+SELECT ?item ?itemLabel ?fatherLabel
+WHERE 
+{
+  ?item wdt:P31 wd:Q5. # instance of human
+  ?item wdt:P27 wd:Q30. # US citizen
+  ?item wdt:P106 wd:Q82955. # politician
+  ?item wdt:P22 ?father. # who have *a* father
+  ?father wdt:P106 wd:Q82955. # who *himself* was a politician
+  
+  ?child wdt:P569 ?dob . # child should have *a* date of birth
+  
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+}
 """
